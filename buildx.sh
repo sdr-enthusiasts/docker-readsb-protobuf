5fe425da86fbc15e8dc929744b0fda24fe6646b9
Mike Nye <mike.nye@gmail.com>
2020-10-29 14:39:04 +0800

    Prepare for beta version

--- buildx.sh
+++ buildx.sh
@@ -4,17 +4,18 @@
 REPO=mikenye
 IMAGE=readsb-protobuf
 PLATFORMS="linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
+LATEST_TAG=beta
 
 docker context use x86_64
 export DOCKER_CLI_EXPERIMENTAL="enabled"
 docker buildx use homecluster
 
 # Build & push latest
-docker buildx build --no-cache -t "${REPO}/${IMAGE}:latest" --compress --push --platform "${PLATFORMS}" .
+docker buildx build --no-cache -t "${REPO}/${IMAGE}:${LATEST_TAG}" --compress --push --platform "${PLATFORMS}" .
 
 # Get readsb version from latest
-docker pull "${REPO}/${IMAGE}:latest"
-VERSION=$(docker run --rm --entrypoint readsb "${REPO}/${IMAGE}:latest" --version | cut -d " " -f 2)
+docker pull "${REPO}/${IMAGE}:${LATEST_TAG}"
+VERSION=$(docker run --rm --entrypoint readsb "${REPO}/${IMAGE}:${LATEST_TAG}" --version | cut -d " " -f 2)
 
 # Build & push version-specific
 docker buildx build -t "${REPO}/${IMAGE}:${VERSION}" --compress --push --platform "${PLATFORMS}" .
@@ -24,7 +25,7 @@ docker buildx build -t "${REPO}/${IMAGE}:${VERSION}" --compress --push --platfor
 sed '/^HEALTHCHECK /d' < Dockerfile > Dockerfile.nohealthcheck
 
 # Build & push latest
-docker buildx build -f Dockerfile.nohealthcheck -t "${REPO}/${IMAGE}:latest_nohealthcheck" --compress --push --platform "${PLATFORMS}" .
+docker buildx build -f Dockerfile.nohealthcheck -t "${REPO}/${IMAGE}:${LATEST_TAG}_nohealthcheck" --compress --push --platform "${PLATFORMS}" .
 
 # If there are version differences, build & push with a tag matching the build date
 docker buildx build -f Dockerfile.nohealthcheck -t "${REPO}/${IMAGE}:${VERSION}_nohealthcheck" --compress --push --platform "${PLATFORMS}" .
