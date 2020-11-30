6210393a9fec3e29135cbc12f0b939b5c9a2aaeb
Mike Nye <mike.nye@gmail.com>
2020-11-30 11:46:46 +0800

    Script updates

--- rootfs/scripts/estimate_rtlsdr_ppm.sh
+++ rootfs/scripts/estimate_rtlsdr_ppm.sh
@@ -12,19 +12,19 @@ TEMPFILE_RTL_TEST_PPM_SCORES=$(mktemp --tmpdir=/tmp tmp.rtl_test.ppm_scores.XXXX
 # Run rtl_test -p for 30 minutes
 echo ""
 echo -e "${WHITE}Running rtl_test -p for 30 minutes${NOCOLOR}"
-timeout 30m rtl_test -p | tee "$TEMPFILE_RTL_TEST_OUTPUT"
+echo ""
+timeout 30m stdbuf -oL rtl_test -p 2>&1 | tee "$TEMPFILE_RTL_TEST_OUTPUT"
 
 # Determine & show results
 echo ""
 echo -e "${WHITE}Results:${NOCOLOR}"
+echo ""
 grep -oP '^real sample rate: \d+ current PPM: \-?\d+ cumulative PPM: \K\-?\d+$' "$TEMPFILE_RTL_TEST_OUTPUT" > "$TEMPFILE_RTL_TEST_PPM_VALUES"
 # shellcheck disable=SC2013
 for i in $(sort -u ppm_values "$TEMPFILE_RTL_TEST_PPM_VALUES"); do
     echo "PPM setting of: $i, Score of: $(grep --count -oP "^$i\$" "$TEMPFILE_RTL_TEST_PPM_VALUES")" >> "$TEMPFILE_RTL_TEST_PPM_SCORES"
 done
-echo -e "${WHITE}"
 sort -nk 7 "$TEMPFILE_RTL_TEST_PPM_SCORES"
-echo -e "${NOCOLOR}"
 
 # Show estimated optimum ppm setting
 echo ""
