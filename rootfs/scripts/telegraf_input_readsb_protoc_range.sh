a49cbbc827291f8cfe4c50f7a5d47cff6a3aefe3
Mathias Bøhn Grytemark <mathias@grytemark.no>
2022-01-14 12:53:58 +0100

    fix variable syntax

--- rootfs/scripts/telegraf_input_readsb_protoc_range.sh
+++ rootfs/scripts/telegraf_input_readsb_protoc_range.sh
@@ -19,7 +19,7 @@ bearing=0
 
 # Pre-fill range array with "0"s to fix https://github.com/mikenye/docker-readsb-protobuf/issues/64
 for key in {71..0}; do
-  range[$key] = 0
+  range[$key]=0
 done
 
 for stat in "${stats_from_protoc[@]}"; do
